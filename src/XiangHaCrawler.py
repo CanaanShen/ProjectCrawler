47a50371842db308633910e658d6465f71af5800
CanaanShen <xyshzjucs@zju.edu.cn>
2016-05-25 10:50:19 +0800

    Move

--- src/XiangHaCrawler.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on 2016Äê5ÔÂ25ÈÕ
-
-@author: Yueshen
-'''
-
-if __name__ == '__main__':
-    
-    print("Program ends")
---
+++ src/crawler/XiangHaCrawler.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+
+'''
+Created on 2016 
+
+@author: Yueshen
+'''
+
+class XiangHaCrawler:
+    
+    def crawlXiangHa(self, rootURL):
+        
+    #def
+#class
+
+if __name__ == '__main__':
+    
+    rootURL = r"http://www.xiangha.com/shicai/"
+    outRootDir = r"../data"
+    
+    print("Program ends")
